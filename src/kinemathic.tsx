66f6e5b55f763bb58091d5f6ff84ec7115d75102
Andrey <smirnov.andrey.serg@gmail.com>
2024-03-30 14:46:01 +0300

    Выделен класс сцены с удобным интерфейсом

--- src/kinemathic.tsx
+++ src/kinemathic.tsx
@@ -1,40 +1,50 @@
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 
-function renderScheme(canvas: HTMLCanvasElement) {
-    // Создаем сцену
-    const scene = new THREE.Scene();
+class Scene {
+    public instance: THREE.Scene;
+    public camera: THREE.PerspectiveCamera;
+    public renderer: THREE.WebGLRenderer
 
-    // Создаем камеру
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
+    constructor(canvas: HTMLCanvasElement) {
+        this.instance = new THREE.Scene();
 
-    // Создаем рендерер
-    const renderer = new THREE.WebGLRenderer({ canvas });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera.position.z = 5;
 
-    // Создаем куб
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    // Функция анимации
-    function animate() {
-        requestAnimationFrame(animate);
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-        renderer.render(scene, camera);
+        this.renderer = new THREE.WebGLRenderer({ canvas });
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
-    animate();
+    public addObject(): void {
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new THREE.Mesh(geometry, material);
+        this.instance.add(cube);
+    }
 
-    // Очистка ресурсов при размонтировании компонента
-    return () => {
-        geometry.dispose();
-        material.dispose();
-        renderer.dispose();
-    };
+    public getObject(objectUUID: string): THREE.Object3D | undefined {
+        return this.instance.getObjectByProperty('uuid', objectUUID);
+    }
+
+    public animate = (): void => {
+        requestAnimationFrame(this.animate);
+        const cube = this.instance.children[0];
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        this.renderer.render(this.instance, this.camera);
+    }
+
+    // TODO(erondondron): Позаботиться об очистке ресурсов
+    // https://threejs.org/docs/index.html#manual/en/introduction/How-to-dispose-of-objects
+    public cleanup(): void { }
+}
+
+function renderScheme(canvas: HTMLCanvasElement) {
+    const scene = new Scene(canvas);
+    scene.addObject();
+    scene.animate();
+    return () => { scene.cleanup(); };
 }
 
 function KinematicPage() {
