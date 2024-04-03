ccfda96f97bc40ea9ebc0ba1ede453a71615e4e2
Andrey <smirnov.andrey.serg@gmail.com>
2024-04-04 02:35:14 +0300

    Реализована отмена изменений проекта

--- src/project/edit.tsx
+++ src/project/edit.tsx
@@ -3,16 +3,31 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import SimulacrumWindow from './simulacrum'
 import { Project } from '../models'
 import { PROJECTS_URL } from '../urls'
+import { plainToClass } from 'class-transformer'
 
 export function SimulacrumEditPage() {
+    const divRef = useRef<HTMLDivElement>(null)
     const navigate = useNavigate()
     const location = useLocation()
-    const project: Project = location.state
-    const divRef = useRef<HTMLDivElement>(null)
+    let project: Project = location.state
+
+    const cancelChanges = async () => {
+        try {
+            const response = await fetch(`${PROJECTS_URL}/${project.uid}`);
+            if (!response.ok) {
+                throw new Error('Не удалось получить исходный проект');
+            }
+            const json = await response.json();
+            project = plainToClass(Project, json);
+            navigate(`/${project.uid}`, { state: project })
+        } catch (error) {
+            console.error('При отмене изменений возникла ошибка: ', error);
+        }
+    }
 
     const deleteProject = async () => {
         try {
-            await fetch(`${PROJECTS_URL}/${project.uid}`, { method: 'DELETE' })
+            fetch(`${PROJECTS_URL}/${project.uid}`, { method: 'DELETE' })
             navigate(`/`)
         } catch (error) {
             console.error('При удалении проекта возникла ошибка: ', error)
@@ -33,7 +48,7 @@ export function SimulacrumEditPage() {
                 <h3>{project.name}</h3>
                 <div className="controlButtons">
                     <button>Save</button>
-                    <button>Cancel</button>
+                    <button onClick={cancelChanges}>Cancel</button>
                     <button onClick={deleteProject}>Delete</button>
                 </div>
             </div>
