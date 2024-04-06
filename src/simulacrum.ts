13424228f1aad97e6f742cb205a325e8513fc266
Andrey <smirnov.andrey.serg@gmail.com>
2024-04-06 16:12:58 +0300

    Добавлено наведение на объекты

--- src/simulacrum.ts
+++ src/simulacrum.ts
@@ -5,18 +5,86 @@ import { plainToClass } from 'class-transformer'
 import { Project, SimulacrumObject, SimulacrumObjectType, SimulacrumState, WebSocketMessage, WSMessageType } from './models'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
+class Object3D {
+    instance: THREE.Object3D
+    bodyGeometry: THREE.BufferGeometry
+    bodyMaterial: THREE.MeshBasicMaterial
+    body: THREE.Mesh
+    edgesGeometry: THREE.EdgesGeometry
+    edgesMaterial: THREE.LineBasicMaterial
+    edges: THREE.LineSegments
+
+    constructor(info: SimulacrumObject) {
+        switch (info.type) {
+            case SimulacrumObjectType.Cube:
+                this.bodyGeometry = new THREE.BoxGeometry(50, 50, 50)
+                break;
+            default:
+                this.bodyGeometry = new THREE.SphereGeometry(25, 16, 16)
+                break;
+        }
+        this.edgesGeometry = new THREE.EdgesGeometry(this.bodyGeometry)
+
+        this.bodyMaterial = new THREE.MeshBasicMaterial({ color: 'white' })
+        this.body = new THREE.Mesh(this.bodyGeometry, this.bodyMaterial)
+
+        this.edgesMaterial = new THREE.LineBasicMaterial({ color: 'black' })
+        this.edges = new THREE.LineSegments(this.edgesGeometry, this.edgesMaterial)
+
+        this.instance = new THREE.Group()
+        this.instance.add(this.body)
+        this.instance.add(this.edges)
+
+        this.setObjectPosition(info)
+    }
+
+    public setObjectPosition(info: SimulacrumObject) {
+        this.instance.position.x = info.position.x
+        this.instance.position.y = info.position.y
+        this.instance.position.z = info.position.z
+        this.instance.rotation.x = info.rotation.x
+        this.instance.rotation.y = info.rotation.y
+        this.instance.rotation.z = info.rotation.z
+    }
+
+    public asSimulacrumObject(): SimulacrumObject {
+        const state = new SimulacrumObject()
+        state.id = this.instance.id
+        const isCube = this.bodyGeometry instanceof THREE.BoxGeometry
+        state.type = isCube ? SimulacrumObjectType.Cube : SimulacrumObjectType.Sphere
+        state.position.x = this.instance.position.x
+        state.position.y = this.instance.position.y
+        state.position.z = this.instance.position.z
+        state.rotation.x = this.instance.rotation.x
+        state.rotation.y = this.instance.rotation.y
+        state.rotation.z = this.instance.rotation.z
+        return state
+    }
+}
+
 class GraphicsWindow {
     protected camera: THREE.OrthographicCamera = new THREE.OrthographicCamera()
     protected renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ antialias: true })
     protected orbitControls: OrbitControls = new OrbitControls(this.camera, this.renderer.domElement)
     protected scene: THREE.Scene = new THREE.Scene()
+    protected objects: Record<number, Object3D> = {}
+
+    protected raycaster = new THREE.Raycaster();
+    protected relPointer: THREE.Vector2 = new THREE.Vector2()
+    protected absPointer: THREE.Vector3 = new THREE.Vector3()
+    protected hoveredObject: Object3D | null = null
+    protected selectedObject: Object3D | null = null
 
     private stepTime: number = 0
     private stepDuration: number = 1000 / 60
 
     constructor() {
         this.scene.background = new THREE.Color(0xfbf0d1)
+        this.renderer.setPixelRatio(window.devicePixelRatio)
         this.setOrbitControls()
+
+        window.addEventListener('mousemove', this.onPointerMove.bind(this));
+        window.addEventListener('wheel', this.onPointerMove.bind(this));
     }
 
     public fitToContainer(container: HTMLDivElement): void {
@@ -36,6 +104,21 @@ class GraphicsWindow {
         requestAnimationFrame(this.animate)
     }
 
+    protected hoverObject(): void {
+        this.raycaster.setFromCamera(this.relPointer, this.camera);
+        if (this.hoveredObject) {
+            this.hoveredObject.bodyMaterial.color.set("white")
+            this.hoveredObject = null
+        }
+        for (const obj of Object.values(this.objects)) {
+            const intersection = this.raycaster.intersectObject(obj.instance);
+            if (intersection.length === 0) continue
+            obj.bodyMaterial.color.set('#f00');
+            this.hoveredObject = obj
+            break
+        }
+    }
+
     protected applayStepChanges(): void { }
 
     protected setOrbitControls(): void {
@@ -46,12 +129,12 @@ class GraphicsWindow {
         }
     }
 
-    private onResizeWindow(event: UIEvent): void {
+    protected onResizeWindow(event: UIEvent): void {
         if (event.target && event.target instanceof HTMLDivElement)
             this.resizeCanvas(event.target)
     }
 
-    private resizeCanvas(container: HTMLDivElement): void {
+    protected resizeCanvas(container: HTMLDivElement): void {
         this.camera.left = container.clientWidth / -2
         this.camera.right = container.clientWidth / 2
         this.camera.top = container.clientHeight / 2
@@ -62,11 +145,30 @@ class GraphicsWindow {
             container.clientHeight,
         )
     }
+
+    protected setPointerPosition(event: MouseEvent) {
+        const containerX = event.clientX - this.renderer.domElement.offsetLeft
+        const containerY = event.clientY - this.renderer.domElement.offsetTop
+
+        const sceneX = (this.camera.left + containerX) / this.camera.zoom
+        const sceneY = (this.camera.top - containerY) / this.camera.zoom
+
+        this.absPointer.x = this.camera.position.x + sceneX
+        this.absPointer.y = this.camera.position.y + sceneY
+
+        // FIXME(erondondron): Неправильно считается при скейлинге или смещении
+        this.relPointer.x = this.absPointer.x / this.camera.right
+        this.relPointer.y = this.absPointer.y / this.camera.top
+    }
+
+    protected onPointerMove(event: MouseEvent): void {
+        this.setPointerPosition(event)
+        this.hoverObject()
+    }
 }
 
 export class SimulacrumWindow extends GraphicsWindow {
     protected eventLoop: Queue<SimulacrumState> = new Queue()
-    protected objects: Record<number, THREE.Object3D> = {}
 
     constructor(protected project: Project) {
         super()
@@ -97,7 +199,8 @@ export class SimulacrumWindow extends GraphicsWindow {
             .then(stateJson => {
                 const state = plainToClass(SimulacrumState, stateJson)
                 for (const objInfo of state.objects) {
-                    const object = this.createObject(objInfo)
+                    const object = new Object3D(objInfo)
+                    this.scene.add(object.instance)
                     this.objects[objInfo.id] = object
                 }
                 this.eventLoop.enqueue(state)
@@ -110,42 +213,6 @@ export class SimulacrumWindow extends GraphicsWindow {
         this.camera.position.z = 2000
     }
 
-    protected createObject(info: SimulacrumObject) {
-        let objectGeometry: THREE.BufferGeometry
-        switch (info.type) {
-            case SimulacrumObjectType.Cube:
-                objectGeometry = new THREE.BoxGeometry(50, 50, 50)
-                break;
-            default:
-                objectGeometry = new THREE.SphereGeometry(25, 16, 16)
-                break;
-        }
-        const edgeGeometry = new THREE.EdgesGeometry(objectGeometry);
-
-        const objectMaterial = new THREE.MeshBasicMaterial({ color: 'white' })
-        const object = new THREE.Mesh(objectGeometry, objectMaterial)
-
-        const edgeMaterial = new THREE.LineBasicMaterial({ color: 'black'})
-        const edges = new THREE.LineSegments(edgeGeometry, edgeMaterial)
-
-        const group = new THREE.Group()
-        group.add(object)
-        group.add(edges)
-
-        this.setObjectPosition(group, info)
-        this.scene.add(group)
-        return group
-    }
-
-    protected setObjectPosition(object: THREE.Object3D, info: SimulacrumObject) {
-        object.position.x = info.position.x
-        object.position.y = info.position.y
-        object.position.z = info.position.z
-        object.rotation.x = info.rotation.x
-        object.rotation.y = info.rotation.y
-        object.rotation.z = info.rotation.z
-    }
-
     protected applayStepChanges(): void {
         if (this.eventLoop.isEmpty())
             return
@@ -153,20 +220,18 @@ export class SimulacrumWindow extends GraphicsWindow {
         const statement = this.eventLoop.dequeue()
         for (const objInfo of statement.objects) {
             const obj = this.objects[objInfo.id]
-            if (obj) this.setObjectPosition(obj, objInfo)
+            if (obj) obj.setObjectPosition(objInfo)
         }
     }
 }
 
 export class EditableSimulacrumWindow extends SimulacrumWindow {
-    protected draggedObject: THREE.Object3D | null = null
+    protected draggedObject: Object3D | null = null
     protected droppedHook: ((value: SimulacrumObjectType | null) => void) | null = null
 
     constructor(project: Project) {
         super(project)
         this.renderer.domElement.addEventListener('click', this.onMouseClicked.bind(this));
-        window.addEventListener('mousemove', this.onMouseMove.bind(this));
-        window.addEventListener('wheel', this.onMouseMove.bind(this));
     }
 
     public setDroppedHook(hook: (value: SimulacrumObjectType | null) => void): void {
@@ -175,20 +240,20 @@ export class EditableSimulacrumWindow extends SimulacrumWindow {
 
     public setDraggedObject(type: SimulacrumObjectType | null) {
         if (this.draggedObject)
-            this.scene.remove(this.draggedObject)
+            this.scene.remove(this.draggedObject.instance)
         if (!type)
             return
         const info = new SimulacrumObject()
         info.type = type
         info.position.x = this.camera.left * 2
         info.position.y = this.camera.top * 2
-        this.draggedObject = this.createObject(info)
-        this.scene.add(this.draggedObject)
+        this.draggedObject = new Object3D(info)
+        this.scene.add(this.draggedObject.instance)
     }
 
     public getState(): SimulacrumState {
         const state = new SimulacrumState()
-        state.objects = Object.values(this.objects).map(item => this.getObjectState(item))
+        state.objects = Object.values(this.objects).map(item => item.asSimulacrumObject())
         return state
     }
 
@@ -210,22 +275,19 @@ export class EditableSimulacrumWindow extends SimulacrumWindow {
     protected onMouseClicked() {
         if (!this.draggedObject)
             return
-        this.objects[this.draggedObject.id] = this.draggedObject
+        this.objects[this.draggedObject.instance.id] = this.draggedObject
         this.draggedObject = null
         if (this.droppedHook)
             this.droppedHook(null)
     }
 
-    protected onMouseMove(event: MouseEvent) {
-        if (!this.draggedObject)
+    protected onPointerMove(event: MouseEvent) {
+        this.setPointerPosition(event)
+        if (!this.draggedObject) {
+            this.hoverObject()
             return
-        const relativeContainerX = event.clientX - this.renderer.domElement.offsetLeft
-        const relativeContainerY = event.clientY - this.renderer.domElement.offsetTop
-
-        const relativeSceneX = (this.camera.left + relativeContainerX) / this.camera.zoom
-        const relativeSceneY = (this.camera.top - relativeContainerY) / this.camera.zoom
-
-        this.draggedObject.position.x = this.camera.position.x + relativeSceneX
-        this.draggedObject.position.y = this.camera.position.y + relativeSceneY
+        }
+        this.draggedObject.instance.position.x = this.absPointer.x
+        this.draggedObject.instance.position.y = this.absPointer.y
     }
 }
